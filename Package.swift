df99cb31e27736e70b47ba4cf090468e57e596db
Simon Nickel <simonnickel@googlemail.com>
2024-11-30 11:24:42 +0100

    Copy Package Template and ajdust package name

--- Package.swift
+++ Package.swift
@@ -4,18 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "SnapStyle",
+    name: "snap-style",
+	platforms: [
+		.iOS(.v18), .macOS(.v15)
+	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SnapStyle",
             targets: ["SnapStyle"]),
     ],
+    // TODO: Remove if not required.
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		.package(url: "https://github.com/simonnickel/snap-foundation.git", branch: "main"),
+	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SnapStyle"),
-
-    ]
+            name: "SnapStyle",
+            dependencies: [ // TODO: Remove if not required.
+				.product(name: "SnapFoundation", package: "snap-foundation"),
+			]
+		),
+    ],
+    swiftLanguageModes: [.version("6")]
 )
---
+++ README.md
@@ -0,0 +1,32 @@
+<!-- Copy badges from SPI -->
+[![](https://img.shields.io/endpoint?url=https%3A%2F%2Fswiftpackageindex.com%2Fapi%2Fpackages%2Fsimonnickel%2Fsnap-style%2Fbadge%3Ftype%3Dplatforms)](https://swiftpackageindex.com/simonnickel/snap-style)
+[![](https://img.shields.io/endpoint?url=https%3A%2F%2Fswiftpackageindex.com%2Fapi%2Fpackages%2Fsimonnickel%2Fsnap-style%2Fbadge%3Ftype%3Dswift-versions)](https://swiftpackageindex.com/simonnickel/snap-style) 
+
+> This package is part of the [SNAP](https://github.com/simonnickel/snap) suite.
+
+
+# SnapStyle
+
+Short description ...
+
+[![Documentation][documentation badge]][documentation] 
+
+[documentation]: https://swiftpackageindex.com/simonnickel/snap-style/main/documentation/snapstyle
+[documentation badge]: https://img.shields.io/badge/Documentation-DocC-blue
+
+
+## Setup
+
+Steps to setup the package ...
+
+
+## Demo project
+
+The [demo project](/PackageDemo) shows ...
+
+<img src="/screenshot.png" height="400">
+
+
+## How to use
+
+Details about package content ...
