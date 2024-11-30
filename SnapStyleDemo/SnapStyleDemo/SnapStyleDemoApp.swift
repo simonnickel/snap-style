81eba7f95b0e3ac8ce7e494ed8465dab2d273fe1
Simon Nickel <simonnickel@googlemail.com>
2024-11-30 11:39:13 +0100

    Create and setup Demo project

---
+++ SnapStyleDemo/SnapStyleDemo/SnapStyleDemoApp.swift
@@ -0,0 +1,15 @@
+//
+//  SNAP - https://github.com/simonnickel/snap
+//  Created by Simon Nickel
+//
+
+import SwiftUI
+
+@main
+struct SnapStyleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapStyleDemo/SnapStyleDemo/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  SNAP - https://github.com/simonnickel/snap
+//  Created by Simon Nickel
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
