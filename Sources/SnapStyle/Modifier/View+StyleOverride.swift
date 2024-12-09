a717ea1489c866d0c0af7ec9e5c8990840879778
Simon Nickel <simonnickel@googlemail.com>
2024-12-09 15:41:59 +0100

    Move code around

---
+++ Sources/SnapStyle/Modifier/View+StyleOverride.swift
@@ -0,0 +1,65 @@
+//
+//  SNAP - https://github.com/simonnickel/snap
+//  Created by Simon Nickel
+//
+
+import SwiftUI
+
+extension View {
+    
+    public func styleOverride(
+        fonts: [SnapStyle.FontKey : SnapStyle.ValueBuilder<SnapStyle.FontKey.Value>]? = nil,
+        surfaces: [SnapStyle.SurfaceKey : SnapStyle.ValueBuilder<SnapStyle.SurfaceKey.Value>]? = nil
+    ) -> some View {
+        self.modifier(
+            StyleOverrideModifier(
+                fonts: fonts,
+                surfaces: surfaces
+            )
+        )
+    }
+    
+}
+
+
+// MARK: - Modifier
+
+private struct StyleOverrideModifier: ViewModifier {
+    
+    @Environment(\.style) private var style
+    
+    let fonts: [SnapStyle.FontKey : SnapStyle.ValueBuilder<SnapStyle.FontKey.Value>]?
+    let surfaces: [SnapStyle.SurfaceKey : SnapStyle.ValueBuilder<SnapStyle.SurfaceKey.Value>]?
+
+    func body(content: Content) -> some View {
+        
+        content
+            .environment(\.style, style.replaced(fonts: fonts, surfaces: surfaces))
+        
+    }
+    
+}
+
+
+// MARK: - SnapStyle: replaced
+
+extension SnapStyle {
+    
+    internal func replaced(
+        fonts: [FontKey : ValueBuilder<FontKey.Value>]? = nil,
+        surfaces: [SurfaceKey : ValueBuilder<SurfaceKey.Value>]? = nil
+    ) -> Self {
+        var style = self
+
+        if let fonts {
+            style.apply(fontBuilder: fonts)
+        }
+
+        if let surfaces {
+            style.apply(surfaceBuilder: surfaces)
+        }
+        
+        return style
+    }
+    
+}
