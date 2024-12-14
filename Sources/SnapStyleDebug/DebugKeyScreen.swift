9ced606415027be307b4108a18cc022f52df1e14
Simon Nickel <simonnickel@googlemail.com>
2024-12-14 20:31:25 +0100

    Add context config

--- Sources/SnapStyleDebug/DebugKeyScreen.swift
+++ Sources/SnapStyleDebug/DebugKeyScreen.swift
@@ -14,17 +14,59 @@ public struct DebugKeyScreen<KeyType: StyleKey>: View {
     
     let keyPath: ItemKeyPath
     
+    @State private var componentType: SnapStyle.Component.ComponentType = .any
+    @State private var componentHierarchy: SnapStyle.Component.Hierarchy = .primary
+    @State private var itemType: SnapStyle.Item.ItemType = .any
+    @State private var itemHierarchy: SnapStyle.Item.Hierarchy = .primary
+    
     public init(keyPath: ItemKeyPath) {
         self.keyPath = keyPath
     }
         
     public var body: some View {
+        VStack {
+            config
+            list
+        }
+    }
+    
+    private var config: some View {
+        VStack {
+            HStack {
+                Picker("Component", selection: $componentType) {
+                    ForEach(SnapStyle.Component.ComponentType.allCases, id: \.self) { component in
+                        Text("\(component)")
+                    }
+                }
+                Picker("Hierarchy", selection: $componentHierarchy) {
+                    ForEach(SnapStyle.Component.Hierarchy.allCases, id: \.self) { hierarchy in
+                        Text("\(hierarchy)")
+                    }
+                }
+            }
+            HStack {
+                Picker("Item", selection: $itemType) {
+                    ForEach(SnapStyle.Item.ItemType.allCases, id: \.self) { item in
+                        Text("\(item)")
+                    }
+                }
+                Picker("Hierarchy", selection: $itemHierarchy) {
+                    ForEach(SnapStyle.Item.Hierarchy.allCases, id: \.self) { hierarchy in
+                        Text("\(hierarchy)")
+                    }
+                }
+            }
+        }
+    }
+    
+    private var list: some View {
         let keyPaths = Array(style[keyPath: keyPath].keys)
         
         return List(keyPaths, id: \.self) { keyPath in
             KeyType.row(keyPath: keyPath)
+                .style(component: componentType, hierarchy: componentHierarchy)
+                .style(item: itemType, hierarchy: itemHierarchy)
         }
-
     }
     
 }
