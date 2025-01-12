ddb6d35ce91851ed9142227bda8bc06fa4896452
Simon Nickel <simonnickel@googlemail.com>
2025-01-12 14:49:47 +0100

    Cleanup

--- Sources/SnapStyleValues/Context/Context.swift
+++ Sources/SnapStyleValues/Context/Context.swift
@@ -7,24 +7,13 @@ extension SnapStyle {
     
     // TODO: Context could be dictionary with extensions to get actual properties.
 
-    public struct Context: Hashable, Sendable, CaseIterable {
+    public struct Context: Hashable, Sendable {
 
         public let component: Component
         public let element: Element
 
         /// A context to use for default values.
         public static let any: SnapStyle.Context = .init(component: .init(type: .any, hierarchy: .primary), element: .init(type: .any, hierarchy: .primary))
-
-        public static let allCases: [SnapStyle.Context] = {
-            var cases: [SnapStyle.Context] = []
-            for component in Component.allCases {
-                for element in Element.allCases {
-                    cases.append(.init(component: component, element: element))
-                }
-            }
-
-            return cases
-        }()
 
     }
 
