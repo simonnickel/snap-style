4a7f234d84e56dd0d58854483117e7fcf766f465
Simon Nickel <simonnickel@googlemail.com>
2025-01-15 15:29:01 +0100

    Add more to example

--- SnapStyleDemo/SnapStyleDemo/Views/ExampleScreens/StructuredTextScreen.swift
+++ SnapStyleDemo/SnapStyleDemo/Views/ExampleScreens/StructuredTextScreen.swift
@@ -11,15 +11,19 @@ struct StructuredTextScreen: View {
     // TODO: Replace spacings with Style Values
     
     var body: some View {
-        StyleVStack(spacing: 16) {
-            content
+        ScrollView {
+            StyleVStack(spacing: 16) {
+                contentText
+                contentList
+                contentButtons
+            }
+            .padding() // TODO: Should not be necessary, could be part of component or screen definition
         }
         .style(component: .content)
-        .frame(maxWidth: .infinity, alignment: .leading)
     }
     
     @ViewBuilder
-    private var content: some View {
+    private var contentText: some View {
         StyleVStack(spacing: 0) {
             Text("Title")
             Text("Subitle")
@@ -38,6 +42,44 @@ struct StructuredTextScreen: View {
         }
     }
     
+    @ViewBuilder
+    private var contentList: some View {
+        // TODO: Style a List without using List
+        StyleVStack {
+            ForEach(0..<3) { index in
+                StyleVStack {
+                    Label("Row \(index)", systemImage: "star")
+                        .padding(5)
+                    Rectangle()
+                        .fill(Color.gray.secondary) // TODO: Style value
+                        .frame(height: 1)
+                }
+            }
+        }
+        .style(component: .list)
+    }
+    
+    @ViewBuilder
+    private var contentButtons: some View {
+        StyleHStack {
+            // TODO: Secondary Button Style
+            Button {} label: {
+                Label("Action", systemImage: "star")
+            }
+            .buttonStyle(.borderless)
+            .style(element: .cta, hierarchy: .secondary)
+            
+            Spacer()
+            
+            // TODO: Primary Button Style
+            Button {} label: {
+                Label("Action", systemImage: "star")
+            }
+            .buttonStyle(.borderedProminent)
+            .style(element: .cta, hierarchy: .primary)
+        }
+    }
+    
     private var paragraph: some View {
         Text("Paragraph with some text to fill a few lines. This is supposed to be a block of text that can be read properly. While the other elements are supposed to provide context and structure to it.")
             .style(element: .content)
@@ -47,5 +89,4 @@ struct StructuredTextScreen: View {
 
 #Preview {
     StructuredTextScreen()
-        .padding() // TODO: Should not be necessary, could be part of component or screen definition
 }
