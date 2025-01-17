1953cfd53536291c1cd0928365f70d612c149781
Simon Nickel <simonnickel@googlemail.com>
2025-01-17 09:42:12 +0100

    Add docu

--- Sources/SnapStyleBase/Values/Base/SnapStyle.Value.swift
+++ Sources/SnapStyleBase/Values/Base/SnapStyle.Value.swift
@@ -5,9 +5,15 @@
 
 extension SnapStyle {
     
+    /// Top level type of value wrapper created by a `ValueBuilder`. It wraps the actual value to allow generic handling of references.
     public enum Value<Key: StyleKey> {
+        
+        /// A reference to another `KeyPath`.
         case reference(Key.ValueBuilderKeyPath)
+        
+        /// A definition of an actual value.
         case definition(Key.Value)
+        
     }
     
 }
