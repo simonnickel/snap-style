b27c1979a792c6e975cd7c6b1ce55ca7121fc6cc
Simon Nickel <simonnickel@googlemail.com>
2025-03-09 21:19:06 +0100

    Add convenience modifier to stretch a view

---
+++ Sources/SnapStyle/CommonViews/View+stretch.swift
@@ -0,0 +1,15 @@
+//
+//  SNAP - https://github.com/simonnickel/snap
+//  Created by Simon Nickel
+//
+
+import SwiftUI
+
+extension View {
+    
+    public func stretch(alignment: Alignment = .leading) -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: alignment)
+    }
+    
+}
