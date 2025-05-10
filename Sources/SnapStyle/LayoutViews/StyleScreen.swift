8654dca1bfc8abc3150ea8de5e10cd50e9857265
Simon Nickel <simonnickel@googlemail.com>
2025-05-10 22:30:30 +0200

    Add StyleScreen.Configuration and fix readable content

--- Sources/SnapStyle/LayoutViews/StyleScreen.swift
+++ Sources/SnapStyle/LayoutViews/StyleScreen.swift
@@ -3,12 +3,14 @@
 //  Created by Simon Nickel
 //
 
+import SnapCore
 import SnapStyleBase
 import SwiftUI
 
 
 // MARK: - StyleScreen
 
+// TODO: Docu no longer fits.
 /// A container to define a Screen.
 /// - Content is stacked vertically in a ScrollView.
 /// - Elements are spaced by`NumberKey.spacingSections`.
@@ -17,40 +19,91 @@ import SwiftUI
 ///     - to limit width to `NumberKey.widthReadableContent`
 ///     - *Caution: content (and background) needs to respect .horizontal safe area insets to comply.*
 /// - By default `Component.screen` is used for styling.
-public struct StyleScreen<Content>: View where Content : View {
+public struct StyleScreen<ScreenContent>: View where ScreenContent : View {
     
     private let component: SnapStyle.ComponentDefinition
-    private let content: () -> Content
+    
+    private let configuration: [Configuration]
+    
+    public typealias ContentBuilder = () -> ScreenContent
+    private let content: ContentBuilder
     
     public init(
         component: SnapStyle.ComponentDefinition = .screen,
-        @ViewBuilder content: @escaping () -> Content
+        configuration: [Configuration] = [.scrollView, .readableContentWidth, .verticalSections],
+        @ViewBuilder content: @escaping () -> ScreenContent
     ) {
         self.component = component
+        self.configuration = configuration
         self.content = content
     }
     
     public var body: some View {
         GeometryReader { geometry in
-            ScrollView {
-                ReadableContentContainer(content: content)
-                    .environment(\.screenGeometrySize, geometry.size)
-            }
-            .frame(maxWidth: .infinity, alignment: .center)
+            createContent()
+                .environment(\.screenGeometrySize, geometry.size)
+                .frame(maxWidth: .infinity, alignment: .center)
         }
         // Using safe area padding to inset the content. Requires the contents background to respect .horizontal safe area insets.
         .style(safeAreaPadding: \.paddingScreenHorizontal, .horizontal)
         .style(safeAreaPadding: \.paddingScreenVertical, .vertical)
         // Background of screen should ignore .vertical safe area to stretch beyond \.`widthReadableContent`.
-        .style(composition: \.screen, ignoreSafeAreaEdges: .vertical)
+        .style(composition: \.screen, ignoreSafeAreaEdges: .vertical) // TODO: Should be the .container composition from given component.
         .style(component: component, applyContainer: nil)
     }
     
     
-    // MARK: Container
+    // MARK: Configuration
     
-    // TODO: Causes the scrollview to put space on top and cut texts, if the inset causes multi line text to change line numbers.
-    struct ReadableContentContainer: View {
+    public enum Configuration {
+        case readableContentWidth
+        case scrollView
+        case verticalSections // TODO: Could be .vertical(spacing:)
+    }
+    
+    @ViewBuilder
+    private func createContent() -> some View {
+        
+        content()
+            .if(configuration.contains(.verticalSections)) { content in
+                content.modifier(ConfigurationModifierVerticalSections())
+            }
+            .if(configuration.contains(.scrollView)) { content in
+                content.modifier(ConfigurationModifierScrollView())
+            }
+            .if(configuration.contains(.readableContentWidth)) { content in
+                content.modifier(ConfigurationModifierReadableContentContainer())
+            }
+    }
+    
+    internal struct ConfigurationModifierReadableContentContainer: ViewModifier {
+        func body(content: Content) -> some View {
+            ReadableContentContainer {
+                content
+            }
+        }
+    }
+    
+    internal struct ConfigurationModifierScrollView: ViewModifier {
+        func body(content: Content) -> some View {
+            ScrollView {
+                content
+            }
+        }
+    }
+    
+    internal struct ConfigurationModifierVerticalSections: ViewModifier {
+        func body(content: Content) -> some View {
+            StyleVStack(spacing: \.spacingSections) {
+                content
+            }
+        }
+    }
+    
+    
+    // MARK: ReadableContentContainer
+    
+    struct ReadableContentContainer<ReadableContent: View>: View {
         
         private struct Constants {
             static var keyPathMaxWidthContent: SnapStyle.NumberKey.ValueBuilderKeyPath { \.widthReadableContent }
@@ -59,7 +112,7 @@ public struct StyleScreen<Content>: View where Content : View {
         @Environment(\.style) private var style
         @Environment(\.screenGeometrySize) private var screenGeometrySize
         
-        let content: () -> Content
+        let content: () -> ReadableContent
         
         var body: some View {
             if let maxWidth = style.number(for: Constants.keyPathMaxWidthContent) {
