dc8d43cafd6db6ae37a3d921f24464a9fd28e69f
Simon Nickel <simonnickel@googlemail.com>
2025-06-04 20:58:25 +0200

    Fix macOS build

--- Sources/SnapStyle/LayoutViews/List/StyleList.swift
+++ Sources/SnapStyle/LayoutViews/List/StyleList.swift
@@ -28,7 +28,9 @@ public struct StyleList<SelectionValue: Hashable, Content: View>: View {
             }
             .styleModify(with: \.spacingSections, transform: { content, value in
                 content
+                #if !os(macOS)
                     .listSectionSpacing(value)
+                #endif
             })
             // TODO FB: Custom Section Spacing does not apply to top of first section. Needs to be smaller than actual section spacing. Would prefer to use `.contentMargins(.top, value, for: .scrollContent)`, but for some weird reason this is applied between first section header and content.
             .style(safeAreaPadding: \.spacingGroups, .top)
