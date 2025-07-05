767b034de91d9d5af68a251cdf432e6f6a7d6c7c
Simon Nickel <simon.nickel@chrono24.com>
2025-07-05 18:33:25 +0200

    Fix padding animation support, add docu and preview

--- Sources/Layout/KeyModifier/Number/PaddingModifier.swift
+++ Sources/Layout/KeyModifier/Number/PaddingModifier.swift
@@ -8,8 +8,15 @@ import SwiftUI
 
 extension View {
 
+    /// Applies the `Number` as `.padding()`
+    ///
+    /// Supports animated change.
+    ///
+    /// - Parameters:
+    ///   - keyPath: The `Number` to apply as length of the padding, `nil` is the inert value
+    ///   - edges: The set of edges to pad for this view. The default is `.all`.
     public func style(
-        padding keyPath: SnapStyle.NumberKey.ValueBuilderKeyPath,
+        padding keyPath: SnapStyle.NumberKey.ValueBuilderKeyPath?,
         _ edges: Edge.Set = .all
     ) -> some View {
         modifier(PaddingModifier(keyPath: keyPath, edges: edges))
@@ -24,18 +31,34 @@ private struct PaddingModifier: ViewModifier {
     
     @Environment(\.style) private var style
     
-    let keyPath: SnapStyle.NumberKey.ValueBuilderKeyPath
+    let keyPath: SnapStyle.NumberKey.ValueBuilderKeyPath?
     let edges: Edge.Set
     
     func body(content: Content) -> some View {
-        if
-            let padding = style.number(for: keyPath)
-        {
-            content
-                .padding(edges, padding)
-        } else {
-            content
-        }
+        let value = style.cgFloat(for: keyPath)
+        content
+            .padding(edges, value)
     }
     
+}
+
+
+// MARK: - Preview
+
+#Preview {
+
+    @Previewable @State var isActive: Bool = true
+
+    Text("Some Preview Content")
+        .style(padding: isActive ? \.spacingSections : nil)
+        .background(.green)
+
+    StyleButton {
+        withAnimation {
+            isActive.toggle()
+        }
+    } content: {
+        Text("Toggle")
+    }
+
 }
