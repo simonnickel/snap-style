f93c72a8682779de411d9a3213268a80d13cb1ce
Simon Nickel <simonnickel@googlemail.com>
2025-09-08 21:22:11 +0200

    Fix iOS 26 separator issue

--- Sources/Layout/LayoutViews/List/StyleList.swift
+++ Sources/Layout/LayoutViews/List/StyleList.swift
@@ -25,15 +25,15 @@ public struct StyleList<SelectionValue: Hashable, Content: View>: View {
     
     public var body: some View {
         StyleScreen(component: .list, configuration: .list) {
-            List {
+            SeparatorReloadingList {
                 content()
                     .styleListRowInsets(\.zero)
             }
+            .listIconWidthScope()
             .styleListSectionSpacing(\.paddingListSection)
             // TODO FB: Custom Section Spacing does not apply to top of first section. Needs to be smaller than actual section spacing. Would prefer to use `.contentMargins(.top, value, for: .scrollContent)`, but for some weird reason this is applied between first section header and content.
             .style(safeAreaPadding: insetTop ? \.paddingListSectionFirst : \.zero, .top)
             .style(safeAreaPadding: \.paddingListBottom, .bottom)
-            .listIconWidthScope()
             // Need to disable to use custom background.
             .scrollContentBackground(.hidden)
             // SwiftUI prevents too small list rows, this messes with .listRowInsets though.
