d625ff6a44edb9390520675fda4e57bb31869b28
Simon Nickel <simonnickel@googlemail.com>
2026-03-14 20:00:23 +0100

    Add missing export

--- Sources/X-SnapStyle/Import.swift
+++ Sources/X-SnapStyle/Import.swift
@@ -4,6 +4,7 @@
 //
 
 @_exported import SnapStyleBase
+@_exported import SnapStyleComponents
 @_exported import SnapStyleDefinitions
 @_exported import SnapStyleModifier
 @_exported import SnapStyleLayout
